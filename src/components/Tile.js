1eb23f4e6f723f97e5d127f35204568edd47b7f7
Duncan06 <tylerdownie0@gmail.com>
2022-03-08 12:47:02 -0800

    Add emoji with click.

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -14,23 +14,42 @@ function Tile() {
 
   function changeGreen() {
     setColor("#2afd22");
+    var elem = document.getElementById("buttonOne");
+    elem.innerHTML = "😁";
+    var elem = document.getElementById("buttonTwo");
+    elem.innerHTML = "I don't know.";
+    var elem = document.getElementById("buttonThree");
+    elem.innerHTML = "Not so great.";
   }
 
   function changeBlue() {
     setColor("#22b8fd");
+    var elem = document.getElementById("buttonOne");
+    elem.innerHTML = "Well!";
+    var elem = document.getElementById("buttonTwo");
+    elem.innerHTML = "🥺";
+    var elem = document.getElementById("buttonThree");
+    elem.innerHTML = "Not so great.";
   }
 
   function changeRed() {
     setColor("#ff7aad");
+    var elem = document.getElementById("buttonOne");
+    elem.innerHTML = "Well!";
+    var elem = document.getElementById("buttonTwo");
+    elem.innerHTML = "I don't know.";
+    var elem = document.getElementById("buttonThree");
+    elem.innerHTML = "😑";
+    
   }
 
   return (
     <div className={classes.tile}>
       <h3>How's it going?</h3>
       <div className={classes.buttons}>
-        <button onClick={changeGreen}>Well!</button>
-        <button onClick={changeBlue}>I don't know.</button>
-        <button onClick={changeRed}>Not so great.</button>
+        <button onClick={changeGreen} id="buttonOne">Well!</button>
+        <button onClick={changeBlue} id="buttonTwo">I don't know.</button>
+        <button onClick={changeRed} id="buttonThree">Not so great.</button>
       </div>
     </div>
   );
